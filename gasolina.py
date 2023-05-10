f416e0c623b4256b24542fe099cfe3454fd1339e
${GITHUB_USER} <${GITHUB_MAIL}>
2023-05-10 10:12:56 -0300

    feat: alteraçõs de gráfico

--- gasolina.py
+++ gasolina.py
@@ -1,15 +1,21 @@
 
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Carrega os dados do arquivo CSV
 df = pd.read_csv('gasolina.csv')
 
-# Define o tamanho do gráfico
-plt.figure(figsize=(10, 6))
+# Define o estilo do gráfico utilizando o Seaborn
+sns.set(style='whitegrid')
 
-# Plota o gráfico de linha com o dia no eixo x e o preço no eixo y
-plt.plot(df['dia'], df['venda'])
+# Cria o gráfico de linha com o dia no eixo x e o preço no eixo y
+plt.figure(figsize=(10, 6))
+sns.lineplot(x='dia', y='venda', data=df, marker='o')
+
+# Adiciona rótulos nos pontos de dados
+for i, row in df.iterrows():
+    plt.text(row['dia'], row['venda'], str(row['venda']), ha='center', va='bottom')
 
 # Define o título do gráfico e dos eixos
 plt.title('Preço da Gasolina por Dia')
